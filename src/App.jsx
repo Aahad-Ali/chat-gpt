fe0817cdca160ee2123b8cc4bc3bfe709db9c1b2
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2023-03-09 04:40:12 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     axios
     // localhost:3000
     // chat-gpt-2-livid.vercel.app
-      .post("http://localhost:5001/chat", { prompt })
+      .post("https://chat-gpt-tau-blond.vercel.app/chat", { prompt })
       .then((res) => {
         // console.log(res.data);
         setResponse(res.data);
